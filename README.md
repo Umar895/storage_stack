ee4338baa6592fd10665673a455ab7e8df78019b
umar895 <m.umar.hameed@outlook.com>
2018-10-23 00:40:37 +0200

    block device initialization

---
+++ README.md
@@ -0,0 +1,6 @@
+
+
+1) The code is followed by the rules of MISRA-C
+http://caxapa.ru/thumbs/468328/misra-c-2004.pdf
+
+
---
+++ src/block_init.c
@@ -0,0 +1,62 @@
+#include<linux/init.h>
+#include<linux/module.h>
+
+
+
+
+/* initialization functio of the blockdevice 
+ * functionality:
+ *	* allocate memory to the device
+ *	* configuring the device
+ *	* init core components
+ */
+static void blockdev_init(struct gendisk *sdisk){
+
+	
+
+
+
+}
+
+
+
+
+
+/* The initialization of the block device in static struct */
+static struct register_device targ_dev = {
+	.name		= "super_ssd",
+	.version 	= {1,0,0},
+	
+	.req_init	= blockdev_req_init,
+	.preset		= blockdev_preset,
+
+	.init		= blockdev_init,
+	.exit		= blockdev_exit,
+
+	.sysfs_init	= blockdev_sysfs_stats_init,
+	.sysfs_exit	= blockdev_sysfs_stats_exit,
+
+	.owner		= THIS_MODULE,
+
+
+};
+
+/* registering the ssd device module */
+static int blockdev_module_init(void){
+
+
+	int ret;
+
+	ret = register_device(&targ_dev);
+
+	return ret;
+	
+}
+
+static void blockdev_module_exit(void){
+
+
+}
+
+module_init(blockdev_module_init);
+module_exit(blockdev_module_exit);
